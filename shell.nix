c30107b5704b96f4588ecc669eaddefd72994eb8
Amy Yan <ayanamydev@gmail.com>
2024-01-01 20:27:54 +1100

    chore: added nix-shell

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import ./pkgs.nix {}, ci ? false }:
+
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    shellcheck
+    gitAndTools.gh
+    rustc
+    cargo
+    cmake
+    # Rust bindgen hook (necessary to build boring)
+    rustPlatform.bindgenHook
+  ];
+  # Don't set rpath for native addons
+  NIX_DONT_SET_RPATH = true;
+  NIX_NO_SELF_RPATH = true;
+  RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+  LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib";
+}
